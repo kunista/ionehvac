64f6ccb9b25a093216fdf985fed74c9580bf3bcf
Roman Orlov <rorlov@wayfair.com>
2017-11-08 09:59:38 -0500

    dfgdfg

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/debug', function () {
-//testinfdfd
+//testing
     $debug = [
         'Environment' => App::environment(),
         'Database defaultStringLength' => Illuminate\Database\Schema\Builder::$defaultStringLength,
